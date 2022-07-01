ea39e942a54df4850ad2f6aff972e592de57af75
Developer95-rod <shevindi.95@outlook.com>
2022-07-01 13:09:16 +0530

    Full project

---
+++ src/main/java/com/example/restfulWebService_app/RestfulWebServiceAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulWebService_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebServiceAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restfulWebService_app/RestfulWebServiceAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restfulWebService_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
